d694c01150ae6df89f972ce45900658cb63684dd
virdi <virdi@192.168.1.162>
2023-01-12 19:53:57 -0500

    Pull From GitHub

--- src/MainDemo.java
+++ src/MainDemo.java
@@ -1,31 +1,31 @@
 
 public class MainDemo {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("GitHub");
-		
+
 		divide(1, 0);
 	}
-	
+
 	public static void add(int a, int b) {
-		
+
 		System.out.println(a + b);
 	}
-	
+
 	public void subtract(int a, int b) {
-		
+
 		System.out.println(a - b);
 	}
-	
+
 	private static void divide(int a, int b) {
-		
+
 		System.out.println(a / b);
 	}
-	
+
 	public int multiply(int a, int b) {
-		
+
 		return a * b;
 
-}
+	}
 }
